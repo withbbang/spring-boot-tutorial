2a8e0fdaa56aeae2882d0a79a6631030741db8e7
김영선 <withbbang@gmail.com>
2024-07-22 18:01:31 +0900

    Feat: First Commit

---
+++ src/main/java/com/tutorial/spring_boot_tutorial/ServletInitializer.java
@@ -0,0 +1,12 @@
+package com.tutorial.spring_boot_tutorial;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootTutorialApplication.class);
+	}
+
+}
