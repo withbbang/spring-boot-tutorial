9771f520ddbfd5a4bd17076af83602a57f7c6c0c
김영선 <withbbang@gmail.com>
2024-08-14 17:20:54 +0900

    Test: 기본 컨트롤러 테스트

---
+++ src/main/java/com/tutorial/spring_boot_tutorial/TestController.java
@@ -0,0 +1,17 @@
+package com.tutorial.spring_boot_tutorial;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "test!";
+    }
+
+    @GetMapping("/good")
+    public String good() {
+        return "good!";
+    }
+}
