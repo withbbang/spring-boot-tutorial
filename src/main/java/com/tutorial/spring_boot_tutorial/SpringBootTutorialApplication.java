c3ad674c84b0df65bd3db9a196f575f6812e9871
김영선 <withbbang@gmail.com>
2024-08-14 17:07:11 +0900

    Test: Java code prettier 테스트

--- src/main/java/com/tutorial/spring_boot_tutorial/SpringBootTutorialApplication.java
+++ src/main/java/com/tutorial/spring_boot_tutorial/SpringBootTutorialApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootTutorialApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootTutorialApplication.class, args);
 	}
-
 }
