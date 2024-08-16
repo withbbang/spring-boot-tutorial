7eb2682d30c77b72a3de3a27674a01a61e8f20be
김영선 <withbbang@gmail.com>
2024-08-16 15:12:00 +0900

    Test: thymeleaf ssr 테스트

--- src/main/java/com/tutorial/spring_boot_tutorial/main/controller/MainViewController.java
+++ src/main/java/com/tutorial/spring_boot_tutorial/main/controller/MainViewController.java
@@ -1,12 +1,15 @@
 package com.tutorial.spring_boot_tutorial.main.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainViewController {
     @GetMapping("/")
-    public String main() {
+    public String main(Model model) {
+        model.addAttribute("main", "Macho");
+
         return "main";
     }
 }
