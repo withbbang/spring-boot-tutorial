73637c5fb49953a63d83ec9131758cb1892c63d7
김영선 <withbbang@gmail.com>
2024-08-20 15:10:01 +0900

    Test: loggin AOP 테스트

---
+++ src/main/java/com/tutorial/spring_boot_tutorial/aspects/LoggingAspect.java
@@ -0,0 +1,76 @@
+package com.tutorial.spring_boot_tutorial.aspects;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspect {
+
+    /**
+     * Before: 대상 “메서드”가 실행되기 전에 Advice를 실행합니다.
+     *
+     * @param joinPoint
+     */
+    @Before("execution(* com.tutorial.spring_boot_tutorial.**.controller.*.*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        log.info("Before: " + joinPoint.getSignature().getName());
+    }
+
+    /**
+     * After : 대상 “메서드”가 실행된 후에 Advice를 실행합니다.
+     *
+     * @param joinPoint
+     */
+    @After("execution(* com.tutorial.spring_boot_tutorial.**.controller.*.*(..))")
+    public void logAfter(JoinPoint joinPoint) {
+        log.info("After: " + joinPoint.getSignature().getName());
+    }
+
+    /**
+     * AfterReturning: 대상 “메서드”가 정상적으로 실행되고 반환된 후에 Advice를 실행합니다.
+     *
+     * @param joinPoint
+     * @param result
+     */
+    @AfterReturning(
+            pointcut = "execution(* com.tutorial.spring_boot_tutorial.**.controller.*.*(..))",
+            returning = "result")
+    public void logAfterReturning(JoinPoint joinPoint, Object result) {
+        log.info("AfterReturning: " + joinPoint.getSignature().getName() + " result: " + result);
+    }
+
+    /**
+     * AfterThrowing: 대상 “메서드에서 예외가 발생”했을 때 Advice를 실행합니다.
+     *
+     * @param joinPoint
+     * @param e
+     */
+    @AfterThrowing(
+            pointcut = "execution(* com.tutorial.spring_boot_tutorial.**.controller.*.*(..))",
+            throwing = "e")
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
+        log.info("AfterThrowing: " + joinPoint.getSignature().getName() + " exception: "
+                + e.getMessage());
+    }
+
+    /**
+     * Around : 대상 “메서드” 실행 전, 후 또는 예외 발생 시에 Advice를 실행합니다.
+     *
+     * @param joinPoint
+     * @return
+     * @throws Throwable
+     */
+    @Around("execution(* com.tutorial.spring_boot_tutorial.**.controller.*.*(..))")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("Around before: " + joinPoint.getSignature().getName());
+        Object result = joinPoint.proceed();
+        log.info("Around after: " + joinPoint.getSignature().getName());
+        return result;
+    }
+
+}
