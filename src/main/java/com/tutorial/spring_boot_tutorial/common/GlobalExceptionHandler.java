24d2546e2087b0567a3d4663a8a2db6bd4362240
김영선 <withbbang@gmail.com>
2024-09-04 15:42:21 +0900

    Style: 전역 에러 처리 핸들러 주석 추가

--- src/main/java/com/tutorial/spring_boot_tutorial/common/GlobalExceptionHandler.java
+++ src/main/java/com/tutorial/spring_boot_tutorial/common/GlobalExceptionHandler.java
@@ -5,6 +5,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+/**
+ * Global Exception Handler
+ */
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
