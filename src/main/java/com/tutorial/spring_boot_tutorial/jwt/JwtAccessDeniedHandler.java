de5c9d61a864ea1c7001cac9d57084397a951463
김영선 <withbbang@gmail.com>
2024-09-06 22:12:28 +0900

    Refactor: 인증 에러 응답 포맷 수정, 인증 에러 로그 수정

--- src/main/java/com/tutorial/spring_boot_tutorial/jwt/JwtAccessDeniedHandler.java
+++ src/main/java/com/tutorial/spring_boot_tutorial/jwt/JwtAccessDeniedHandler.java
@@ -6,6 +6,9 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tutorial.spring_boot_tutorial.common.CodeMessage;
+import com.tutorial.spring_boot_tutorial.common.Result;
+import com.tutorial.spring_boot_tutorial.common.SingleResponse;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -24,12 +27,16 @@ public class JwtAccessDeniedHandler implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response,
             AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        log.info(
+                "\n############################################################################## START ##############################################################################");
         log.error("No Authorities", accessDeniedException);
+        log.info(
+                "\n############################################################################### END ###############################################################################");
 
-        JwtErrorResponse jwtErrorResponse = new JwtErrorResponse(HttpStatus.FORBIDDEN.value(),
-                accessDeniedException.getMessage(), LocalDateTime.now());
+        SingleResponse errorResponse = new SingleResponse();
+        errorResponse.setResult(new Result(CodeMessage.ER0001));
 
-        String responseBody = objectMapper.writeValueAsString(jwtErrorResponse);
+        String responseBody = objectMapper.writeValueAsString(errorResponse);
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(HttpStatus.FORBIDDEN.value());
         response.setCharacterEncoding("UTF-8");
